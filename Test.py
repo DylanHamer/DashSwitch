846bdc664298b91080b1046c00bb1adb299a50f4
Dylan Hamer <dylanhamer13@gmail.com>
2018-02-10 00:17:58 +0000

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,13 @@
+from scapy.all import *
+
+mac = ""
+
+def arp_display(pkt):
+  if pkt[ARP].op == 1: #who-has (request)
+    if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
+      if pkt[ARP].hwsrc == mac:
+        print "Pushed"
+      else:
+        print "ARP Probe from unknown device: " + pkt[ARP].hwsrc
+
+print sniff(prn=arp_display, filter="arp", store=0, count=10)
